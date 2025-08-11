2e338731919a5aefa2ed7b8974b328eecb38ac45
Okunichiyou <seifuzi@icloud.com>
2025-08-11 15:07:31 +0900

    ButtonComponentの追加(#12)

---
+++ app/components/ui/button_component.rb
@@ -0,0 +1,42 @@
+module Ui
+  class ButtonComponent < Ui::Base
+    CATEGORY_OPTIONS = %i[primary secondary].freeze
+    SIZE_OPTIONS = %i[full large medium small].freeze
+    TYPE_OPTIONS = %i[button submit reset].freeze
+    VARIANT_OPTIONS = %i[default danger].freeze
+
+    def initialize(
+      category:,
+      button_class: "",
+      size:,
+      type: :button,
+      variant: :default,
+      text:,
+      html_options: {}
+    )
+      @category = filter_attribute(value: category, white_list: CATEGORY_OPTIONS)
+      @button_class = button_class.split
+      @size = filter_attribute(value: size, white_list: SIZE_OPTIONS)
+      @type = filter_attribute(value: type, white_list: TYPE_OPTIONS)
+      @variant = filter_attribute(value: variant, white_list: VARIANT_OPTIONS)
+      @text = text
+      @html_options = build_html_options(html_options)
+    end
+
+    private
+
+    def build_html_options(html_options)
+      options = html_options.merge({ class: button_classes })
+      options.merge!({ type: @type.to_s })
+      super(options)
+    end
+
+    def button_classes
+      classes = [ "button-component" ]
+      classes.push(@category.to_s)
+      classes.concat(@button_class)
+      classes.push(@size.to_s)
+      classes.push(@variant.to_s)
+    end
+  end
+end
---
+++ test/components/ui/button_component_test.rb
@@ -0,0 +1,31 @@
+require "test_helper"
+
+class ButtonComponentTest < ViewComponent::TestCase
+  # =====================================
+  # #html属性
+  # =====================================
+  test "デフォルトの属性が設定され、渡したカテゴリ/テキストが反映されること" do
+    render_inline(Ui::ButtonComponent.new(category: :secondary, size: :medium, text: "test"))
+
+    # button要素とクラスの確認
+    assert_selector("button.button-component.secondary.medium.default[data-scope=ui-button-component]")
+
+    # glass effectの要素がbutton-component直下にあることを確認
+    assert_selector(".button-component > .glass-effect")
+    assert_selector(".button-component > .glass-tint")
+    assert_selector(".button-component > .glass-shine")
+    assert_selector(".button-component > .glass-text", text: "test")
+  end
+
+  test "渡したdisabledが反映されること" do
+    render_inline(Ui::ButtonComponent.new(category: :secondary, size: :medium, text: "test", html_options: { disabled: true }))
+
+    assert_selector("button.button-component.secondary.medium.default[data-scope=ui-button-component][disabled]")
+  end
+
+  test "渡したクラスが反映されること" do
+    render_inline(Ui::ButtonComponent.new(category: :secondary, size: :medium, text: "test", button_class: "added added2"))
+
+    assert_selector("button.button-component.secondary.medium.default.added.added2[data-scope=ui-button-component]")
+  end
+end
