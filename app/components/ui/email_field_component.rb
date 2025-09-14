baa016a6d91360cbb9ab8528127beed2468c00af
Okunichiyou <seifuzi@icloud.com>
2025-09-14 17:37:27 +0900

    EmailFieldComponentを作成(#21)

---
+++ app/components/ui/email_field_component.rb
@@ -0,0 +1,34 @@
+module Ui
+  class EmailFieldComponent < Ui::Base
+    SIZE_OPTIONS = %i[full large medium small].freeze
+
+    def initialize(
+      builder:,
+      method:,
+      size:,
+      html_options: {}
+    )
+      @builder = builder
+      @method = method
+      @size = filter_attribute(value: size, white_list: SIZE_OPTIONS)
+      @html_options = build_html_options(html_options)
+    end
+
+    private
+
+    def build_html_options(html_options)
+      html_options.merge({ class: email_field_classes(html_options) })
+    end
+
+    def email_field_classes(html_options)
+      classes = []
+      classes.push("text-field-component")
+      classes.push(@size.to_s)
+
+      argument_classes = html_options[:class]
+      classes.concat(argument_classes.split) if argument_classes.present?
+
+      classes
+    end
+  end
+end
---
+++ test/components/ui/email_field_component_test.rb
@@ -0,0 +1,45 @@
+require "test_helper"
+
+class Ui::EmailFieldComponentTest < ViewComponent::TestCase
+  def form_builder
+    ActionView::Helpers::FormBuilder.new("user_database_authentication", User::DatabaseAuthentication.new, vc_test_controller.view_context, {})
+  end
+
+  test "基本的な email_field が生成されること" do
+    render_inline(Ui::EmailFieldComponent.new(builder: form_builder, method: :email, size: :medium))
+
+    assert_selector("input[type='email'][name='user_database_authentication[email]'][id='user_database_authentication_email']")
+  end
+
+  test "size のクラスが設定されること" do
+    render_inline(Ui::EmailFieldComponent.new(builder: form_builder, method: :email, size: :large))
+
+    assert_selector("input.text-field-component.large")
+  end
+
+  test "html_options が適用されること" do
+    component = Ui::EmailFieldComponent.new(
+      builder: form_builder,
+      method: :email,
+      size: :medium,
+      html_options: { autofocus: true, autocomplete: "email", class: "custom-class" }
+    )
+
+    render_inline(component)
+
+    # 基本的なinput要素があることを確認
+    assert_selector("input[type='email']")
+    # autofocusとautocomplete属性があることを確認
+    assert_selector("input[autofocus][autocomplete='email']")
+    # classがマージされていることを確認
+    assert_selector("input.text-field-component.medium")
+    # custom-classも含まれていることを確認
+    assert_selector("input.custom-class")
+  end
+
+  test "不適切なsizeを適用したらエラーが出ること" do
+    assert_raises(ArgumentError, "Invalid attribute value: 'invalid'. Must be one of full, large, medium, small.") do
+      Ui::EmailFieldComponent.new(builder: form_builder, method: :email, size: :invalid)
+    end
+  end
+end
