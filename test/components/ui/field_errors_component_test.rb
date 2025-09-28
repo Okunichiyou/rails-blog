5f5a5570555cf892140647fdadcb13a0fd4d3127
Okunichiyou <seifuzi@icloud.com>
2025-09-28 22:43:40 +0900

    EmailFieldComponentにエラー発生時のスタイルを追加し、エラー表示用のフィールドを定義(#25)

---
+++ test/components/ui/field_errors_component_test.rb
@@ -0,0 +1,30 @@
+require "test_helper"
+
+class Ui::FieldErrorsComponentTest < ViewComponent::TestCase
+  test "エラーメッセージがない場合は何も表示されないこと" do
+    render_inline(Ui::FieldErrorsComponent.new(error_messages: []))
+
+    assert_no_selector("ul")
+  end
+
+  test "単一のエラーメッセージが表示されること" do
+    render_inline(Ui::FieldErrorsComponent.new(error_messages: ["Email can't be blank"]))
+
+    assert_selector("ul li", text: "Email can't be blank")
+  end
+
+  test "複数のエラーメッセージがリストで表示されること" do
+    error_messages = ["Email can't be blank", "Email is invalid"]
+    render_inline(Ui::FieldErrorsComponent.new(error_messages: error_messages))
+
+    assert_selector("ul li", count: 2)
+    assert_selector("ul li", text: "Email can't be blank")
+    assert_selector("ul li", text: "Email is invalid")
+  end
+
+  test "data-scopeが適用されること" do
+    render_inline(Ui::FieldErrorsComponent.new(error_messages: ["Email can't be blank"]))
+
+    assert_selector("ul[data-scope]")
+  end
+end
