66cfb19dfad08771eed28c69641ae47326219de1
Okunichiyou <seifuzi@icloud.com>
2025-10-13 22:18:09 +0900

    パスワードフィールドにエラー時の色が付くように修正(#27)

--- app/components/ui/password_field_component.rb
+++ app/components/ui/password_field_component.rb
@@ -1,16 +1,19 @@
 module Ui
   class PasswordFieldComponent < Ui::Base
     SIZE_OPTIONS = %i[full large medium small].freeze
+    VARIANT_OPTIONS = %i[default alert].freeze
 
     def initialize(
       builder:,
       method:,
       size:,
+      variant: :default,
       html_options: {}
     )
       @builder = builder
       @method = method
       @size = filter_attribute(value: size, white_list: SIZE_OPTIONS)
+      @variant = filter_attribute(value: variant, white_list: VARIANT_OPTIONS)
       @html_options = build_html_options(html_options)
     end
 
@@ -24,6 +27,7 @@ module Ui
       classes = []
       classes.push("text-field-component")
       classes.push(@size.to_s)
+      classes.push(@variant.to_s)
 
       argument_classes = html_options[:class]
       classes.concat(argument_classes.split) if argument_classes.present?
