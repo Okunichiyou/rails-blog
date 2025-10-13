d8e6490ebf748eae4d06cc9ce47c7543d5cd7ac5
Okunichiyou <seifuzi@icloud.com>
2025-10-13 22:14:34 +0900

    TextFieldComponentを作成(#27)
    一般的なtextタイプ(emailタイプとかではない)のコンポーネントを作成。
    テキストフィールドはタイプによって、必要なhtml要素とか変わるので、コンポーネントは共通化しない方針なので新しく追加

---
+++ app/components/ui/text_field_component.rb
@@ -0,0 +1,38 @@
+module Ui
+  class TextFieldComponent < Ui::Base
+    SIZE_OPTIONS = %i[full large medium small].freeze
+    VARIANT_OPTIONS = %i[default alert].freeze
+
+    def initialize(
+      builder:,
+      method:,
+      size:,
+      variant: :default,
+      html_options: {}
+    )
+      @builder = builder
+      @method = method
+      @size = filter_attribute(value: size, white_list: SIZE_OPTIONS)
+      @variant = filter_attribute(value: variant, white_list: VARIANT_OPTIONS)
+      @html_options = build_html_options(html_options)
+    end
+
+    private
+
+    def build_html_options(html_options)
+      html_options.merge({ class: text_field_classes(html_options) })
+    end
+
+    def text_field_classes(html_options)
+      classes = []
+      classes.push("text-field-component")
+      classes.push(@size.to_s)
+      classes.push(@variant.to_s)
+
+      argument_classes = html_options[:class]
+      classes.concat(argument_classes.split) if argument_classes.present?
+
+      classes
+    end
+  end
+end
