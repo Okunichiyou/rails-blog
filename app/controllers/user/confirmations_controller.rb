8d0af20e65eae896d706d4563c12605a68b023a7
Okunichiyou <seifuzi@icloud.com>
2025-10-20 21:46:52 +0900

    トークン認証失敗時にエラーメッセージを表示するように修正(#41)

--- app/controllers/user/confirmations_controller.rb
+++ app/controllers/user/confirmations_controller.rb
@@ -17,7 +17,12 @@ class User::ConfirmationsController < Devise::ConfirmationsController
 
   def show
     self.resource = resource_class.confirm_by_token(params[:confirmation_token])
-    redirect_to new_user_database_authentication_path(confirmation_token: resource.confirmation_token)
+
+    if resource.errors.empty?
+      redirect_to new_user_database_authentication_path(confirmation_token: resource.confirmation_token)
+    else
+      respond_with(resource, status: :unprocessable_entity)
+    end
   end
 
   def sent
