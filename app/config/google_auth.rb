21c5e58a7c3394ddcbd4985ddeffa400018550fa
Okunichiyou <seifuzi@icloud.com>
2025-11-23 21:41:20 +0900

    不要と判断したConfig層を削除
    GoogleAuthはinitializer配下で呼び出す必要があり、わざわざinitializerで呼ぶためにrequire_loadするのは過剰なので削除

--- app/config/google_auth.rb
+++
@@ -1,8 +0,0 @@
-class GoogleAuth < ApplicationConfig
-  attr_reader :client_id, :client_secret
-
-  def initialize
-    @client_id = config[:client_id]
-    @client_secret = config[:client_secret]
-  end
-end
