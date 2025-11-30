355444953c0d5fbdb992503bca91bd886a687b3e
Okunichiyou <seifuzi@icloud.com>
2025-11-30 18:19:09 +0900

    viewから直接パスヘルパーを呼び出した方が一貫性があるのでコンポーネントからロジックを削除(#69)

--- app/components/domain/user/account_settings_component.rb
+++ app/components/domain/user/account_settings_component.rb
@@ -5,11 +5,6 @@ module Domain
       def initialize(user:)
         @user = user
       end
-
-      # @rbs () -> String
-      def google_oauth2_path
-        sns_credential_google_oauth2_omniauth_authorize_path
-      end
     end
   end
 end
