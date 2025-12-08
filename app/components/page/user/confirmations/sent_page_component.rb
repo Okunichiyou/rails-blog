a5b7bbd454d19dcfe8a9983005d562d593a1acba
Okunichiyou <seifuzi@icloud.com>
2025-12-08 19:03:12 +0900

    SentPageComponentのカスタムHTML属性を削除(#70)

--- app/components/page/user/confirmations/sent_page_component.rb
+++ app/components/page/user/confirmations/sent_page_component.rb
@@ -2,9 +2,6 @@ module Page
   module User
     module Confirmations
       class SentPageComponent < ApplicationComponent
-        def initialize(**html_options)
-          @html_options = html_options
-        end
       end
     end
   end
