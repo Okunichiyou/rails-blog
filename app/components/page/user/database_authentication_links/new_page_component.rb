03fee7ab43803eed7ab93602373f693256c399e8
Okunichiyou <seifuzi@icloud.com>
2025-12-08 19:08:24 +0900

    DatabaseAuthenticationLinks::NewPageComponentのカスタムHTML属性を削除(#70)

--- app/components/page/user/database_authentication_links/new_page_component.rb
+++ app/components/page/user/database_authentication_links/new_page_component.rb
@@ -2,11 +2,10 @@ module Page
   module User
     module DatabaseAuthenticationLinks
       class NewPageComponent < ApplicationComponent
-        # @rbs (form: User::DatabaseAuthenticationLinkForm, create_database_authentication_link_path: String, **nil) -> void
-        def initialize(form:, create_database_authentication_link_path:, **html_options)
+        # @rbs (form: User::DatabaseAuthenticationLinkForm, create_database_authentication_link_path: String) -> void
+        def initialize(form:, create_database_authentication_link_path:)
           @form = form
           @create_database_authentication_link_path = create_database_authentication_link_path
-          @html_options = html_options
         end
       end
     end
