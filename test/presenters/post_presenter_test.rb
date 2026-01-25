e35ed0b6f5618ab7d45f80a05d575bd0274fd67c
Okunichiyou <seifuzi@icloud.com>
2026-01-25 17:44:18 +0900

    一覧画面で冒頭100文字を表示するように修正(#127)

---
+++ test/presenters/post_presenter_test.rb
@@ -0,0 +1,31 @@
+require "test_helper"
+
+class PostPresenterTest < ActiveSupport::TestCase
+  test "100文字以上の場合、末尾が「...」でtruncateされること" do
+    content = "a" * 101
+    result = PostPresenter.beginning_of_content(content: content)
+
+    assert_equal 100, result.length
+    assert_equal "a" * 97 + "...", result
+  end
+
+  test "100文字の場合、そのまま表示されること" do
+    content = "a" * 100
+    result = PostPresenter.beginning_of_content(content: content)
+
+    assert_equal content, result
+  end
+
+  test "nilを渡した場合、空文字が返ること" do
+    result = PostPresenter.beginning_of_content(content: nil)
+
+    assert_equal "", result
+  end
+
+  test "HTMLタグが除去された状態で表示されること" do
+    content = "<p>Hello</p><strong>World</strong>"
+    result = PostPresenter.beginning_of_content(content: content)
+
+    assert_equal "HelloWorld", result
+  end
+end
